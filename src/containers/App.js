e8bac35f1a344d4d99035c0bde3d587738c302b4
ines-regi <2003inesregina@gmail.com>
2024-01-21 09:25:02 +0000

    corrigidos erros e adicionados ficheiros e dependencias necessarios

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import CardList from '../components/CardList';
-import SearchBox from '../SearchBox';
-import Scroll from '../Scroll';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 import './App.css';
 
 
