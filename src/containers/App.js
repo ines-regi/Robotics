b993e294460e258d1265e9481ea1ef2422c5de2b
ines-regi <2003inesregina@gmail.com>
2024-01-21 08:59:27 +0000

    atualização after seeing the classes on Udemy

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import CardList from '../components/CardList';
-import SearchBox from '../components/SearchBox';
-import Scroll from '../components/Scroll';
+import SearchBox from '../SearchBox';
+import Scroll from '../Scroll';
 import './App.css';
 
 
@@ -29,17 +29,19 @@ class App extends Component {
     const filteredRobots = robots.filter(robot =>{
       return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     })
-    return !robots.length ?
-      <h1>Loading</h1> :
-      (
-        <div className='tc'>
-          <h1 className='f1'>RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange}/>
-          <Scroll>
-            <CardList robots={filteredRobots} />
-          </Scroll>
-        </div>
-      );
+    if(robots.length===0){
+      return ( <h1>Loading</h1>)
+    } else{
+        return (
+          <div className='tc'>
+            <h1 className='f1'>RoboFriends</h1>
+            <SearchBox searchChange={this.onSearchChange}/>
+            <Scroll>
+              <CardList robots={filteredRobots} />
+            </Scroll>
+          </div>
+        );
+      }
   }
 }
 
